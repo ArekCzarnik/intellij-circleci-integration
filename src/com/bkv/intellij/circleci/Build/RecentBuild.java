796f143d5136afdb4eaead7f95c3be5b49b0a1c8
Joost van Driel <joostvdriel@gmail.com>
2017-06-02 15:06:43 +0200

    [CI-1] Added RecentBuildInterface and tests

--- src/com/bkv/intellij/circleci/Build/RecentBuild.java
+++ src/com/bkv/intellij/circleci/Build/RecentBuild.java
@@ -1,4 +1,93 @@
 package com.bkv.intellij.circleci.Build;
 
-public class RecentBuild {
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.Date;
+
+public class RecentBuild implements BuildInterface {
+    @SerializedName("build_num")
+    private Integer buildNumber;
+
+    @SerializedName("vcs_revision")
+    private String vcsRevision;
+
+    @SerializedName("committer_name")
+    private String committerName;
+
+    @SerializedName("committer_date")
+    private Date committerDate;
+
+    @SerializedName("status")
+    private String status;
+
+    @SerializedName("subject")
+    private String subject;
+
+    @SerializedName("outcome")
+    private String outcome;
+
+    @SerializedName("build_url")
+    private String buildUrl;
+
+    public RecentBuild(
+            int buildNumber,
+            String vcsRevision,
+            String committerName,
+            Date committerDate,
+            String status,
+            String subject,
+            String outcome,
+            String buildUrl
+    ) {
+        this.buildNumber = buildNumber;
+        this.vcsRevision = vcsRevision;
+        this.committerName = committerName;
+        this.committerDate = committerDate;
+        this.status = status;
+        this.subject = subject;
+        this.outcome = outcome;
+        this.buildUrl = buildUrl;
+    }
+
+    @Override
+    public Integer getBuildNumber() {
+        return buildNumber;
+    }
+
+    @Override
+    public String getVcsRevision() {
+        return vcsRevision;
+    }
+
+    @Override
+    public String getCommitterName() {
+        return committerName;
+    }
+
+    @Override
+    public Date getCommitterDate() {
+        return committerDate;
+    }
+
+    @Override
+    public String getStatus() {
+        return status;
+    }
+
+    @Override
+    public String getSubject() {
+        return subject;
+    }
+
+    @Override
+    public String getOutcome() {
+        return outcome;
+    }
+
+    @Override
+    public String getBuildUrl() {
+        return buildUrl;
+    }
 }
+
---
+++ tests/com/bkv/intellij/circleci/Build/RecentBuildTest.java
@@ -0,0 +1,32 @@
+package com.bkv.intellij.circleci.Build;
+
+import com.google.gson.Gson;
+import junit.framework.TestCase;
+import org.junit.Assert;
+
+import java.util.Date;
+
+public class RecentBuildTest extends TestCase {
+    private RecentBuild build;
+
+    public void testCanDeserialize() {
+        Assert.assertEquals(new Integer(10), build.getBuildNumber());
+        Assert.assertEquals("a09323cd987", build.getVcsRevision());
+        Assert.assertEquals("Tinus Tester", build.getCommitterName());
+        Assert.assertEquals(new Date("Sat, 01 Jan 2000 00:00:00 GMT+001"), build.getCommitterDate());
+        Assert.assertEquals("success", build.getStatus());
+        Assert.assertEquals("Test subject", build.getSubject());
+        Assert.assertEquals("success", build.getOutcome());
+        Assert.assertEquals("http://url.com", build.getBuildUrl());
+    }
+
+    protected void setUp() {
+        Gson gson = new Gson();
+        build = gson.fromJson("{\"build_num\":\"10\", \"vcs_revision\":\"a09323cd987\"," +
+                        "\"committer_name\": \"Tinus Tester\", \"committer_date\": " +
+                        "\"2000-01-01\", \"status\": \"success\", \"subject\": \"Test subject\"," +
+                        "\"outcome\": \"success\", \"build_url\": \"http://url.com\"}",
+                RecentBuild.class
+        );
+    }
+}
