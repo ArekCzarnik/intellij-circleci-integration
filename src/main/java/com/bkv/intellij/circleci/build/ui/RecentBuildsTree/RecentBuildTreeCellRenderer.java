e94e2be041cbbde4680f9a122b143116b62381f0
Gillis Heevel <g.heevel@bkvgroup.eu>
2017-06-09 20:15:51 +0200

    [CI-15] Changed icon per build status

--- src/main/java/com/bkv/intellij/circleci/build/ui/RecentBuildsTree/RecentBuildTreeCellRenderer.java
+++ src/main/java/com/bkv/intellij/circleci/build/ui/RecentBuildsTree/RecentBuildTreeCellRenderer.java
@@ -15,7 +15,7 @@ public class RecentBuildTreeCellRenderer extends DefaultTreeCellRenderer {
         Object o = ((DefaultMutableTreeNode) value).getUserObject();
         if (o instanceof BuildInterface) {
             BuildInterface build = (BuildInterface) o;
-            setText("[" + build.getStatus() + "] #" + build.getBuildNumber() + ' ' + build.getSubject());
+            setText("#" + build.getBuildNumber() + ' ' + build.getSubject());
             setToolTipText("By " + build.getCommitterName() + " on " + build.getCommitterDate() + "\nVCS Revision was " + build.getVcsRevision());
             setIcon(getIcon(build));
         }
@@ -25,8 +25,19 @@ public class RecentBuildTreeCellRenderer extends DefaultTreeCellRenderer {
     private ImageIcon getIcon(BuildInterface build) {
         ImageIcon icon;
         switch (build.getStatus()) {
+            case "fixed":
+            case "success":
+                icon = new ImageIcon(getClass().getResource("/circleci/010-checked.png").getPath());
+            break;
+            case "running":
+                icon = new ImageIcon(getClass().getResource("/circleci/003-run.png").getPath());
+            break;
+            case "failed":
+                icon = new ImageIcon(getClass().getResource("/circleci/009-cancel.png").getPath());
+            break;
             default:
-                icon = new ImageIcon(getClass().getResource("/circleci/008-error.png").getPath());
+                icon = new ImageIcon(getClass().getResource("/circleci/002-settings.png").getPath());
+            break;
         }
         return icon;
     }
