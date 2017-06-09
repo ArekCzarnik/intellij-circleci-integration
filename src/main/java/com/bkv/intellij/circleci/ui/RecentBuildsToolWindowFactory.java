6cf00bc8328c866a97e2cb429df3e40b20c3bab8
Gillis Heevel <g.heevel@bkvgroup.eu>
2017-06-09 15:07:13 +0200

    Merged plugin.xml

---
+++ src/main/java/com/bkv/intellij/circleci/ui/RecentBuildsToolWindowFactory.java
@@ -0,0 +1,38 @@
+package com.bkv.intellij.circleci.ui;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ToolWindowFactory;
+import com.intellij.ui.content.Content;
+import com.intellij.ui.content.ContentFactory;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+
+public class RecentBuildsToolWindowFactory implements ToolWindowFactory {
+
+    private JTree tree1;
+    private JPanel pnlMain;
+
+    @Override
+    public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
+        ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
+        Content content = contentFactory.createContent(pnlMain, "", false);
+        toolWindow.getContentManager().addContent(content);
+    }
+
+    @Override
+    public void init(ToolWindow window) {
+
+    }
+
+    @Override
+    public boolean shouldBeAvailable(@NotNull Project project) {
+        return false;
+    }
+
+    @Override
+    public boolean isDoNotActivateOnStart() {
+        return false;
+    }
+}
